f65b0f1f3b7437e6c13de6ab6266506e1e1c025f
Jonathan Taylor <jonathan@novoda.com>
2018-07-13 12:32:38 +0100

    adding some more fields

--- Public/index.js
+++ Public/index.js
@@ -11,12 +11,15 @@ const app = flamelink({
 });
 
 window.submitClick = function() {
-  var messageText = document.getElementById("clipURL").value;
+  var clipURL = document.getElementById("clipURL").value;
   var categoryId = document.getElementById("selectCategory").value;
+  var nomineeText = document.getElementById("nominee").value;
+  var showText = document.getElementById("show").value;
+  var seasonText = document.getElementById("season").value;
 
   // To create a new clip entry, use uniqid() for the documentID (arg 2)
   // to update and existing clip field value, use the existing documentID
-  app.content.set('clips', uniqid(), { url: messageText, category: categoryId, score: '1'  })
+  app.content.set('clips', uniqid(), { url: clipURL, category: categoryId, nominee: nomineeText, show: showText, season: seasonText, score: '1'  })
     .then(() => console.log('Updating the entry succeeded'))
     .catch(() => console.error('Something went wrong while updating the entry.'));
 
--- Public/index_browserfyBundle.js
+++ Public/index_browserfyBundle.js
@@ -25178,18 +25178,16 @@ const app = flamelink({
   messagingSenderId: "731501145424"
 });
 
-// var clipURL = document.getElementById("clipURL");
-// var clipCategory = document.getElementById("selectCategory");
-// var submitButton = document.getElementById("submitButton");
-
 window.submitClick = function() {
-  var messageText = document.getElementById("clipURL").value;
+  var clipURL = document.getElementById("clipURL").value;
   var categoryId = document.getElementById("selectCategory").value;
-  // window.alert("URL:" + messageText + "\nCat:" + categoryId);
+  var nomineeText = document.getElementById("nominee").value;
+  var showText = document.getElementById("show").value;
+  var seasonText = document.getElementById("season").value;
 
   // To create a new clip entry, use uniqid() for the documentID (arg 2)
   // to update and existing clip field value, use the existing documentID
-  app.content.set('clips', uniqid(), { url: messageText, category: categoryId, score: '1'  })
+  app.content.set('clips', uniqid(), { url: clipURL, category: categoryId, nominee: nomineeText, show: showText, season: seasonText, score: '1'  })
     .then(() => console.log('Updating the entry succeeded'))
     .catch(() => console.error('Something went wrong while updating the entry.'));
 
